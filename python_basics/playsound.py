d91645be2913c7bc78e3823d87b409b62fc05691
Tim Schoen <timschoen123@gmail.com>
2018-11-02 14:41:00 +0100

    Option to play audio multiple times

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,9 @@
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("audio.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
+def play(audiofile,n):
+    wave_obj = sa.WaveObject.from_wave_file(audiofile)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    if n=0:
+        return()
+        play(audiofile , n - 1)
