ae3a8f94d42c9708de54091a40f2586c12c88636
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2019-09-08 12:39:54 +0200

    Improved code, added comments

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,10 +1,10 @@
 import simpleaudio as sa
 
+#Play function
 def play(n, audiofile="audio.wav"):
-    if n == 0:
-        return()
-    else:
-        wave_obj = sa.WaveObject.from_wave_file(audiofile)
+    # Load audio file
+    wave_obj = sa.WaveObject.from_wave_file(audiofile)
+    # Loop play function n times
+    for i in range(n):
         play_obj = wave_obj.play()
         play_obj.wait_done()
-        play(n - 1 , audiofile)
